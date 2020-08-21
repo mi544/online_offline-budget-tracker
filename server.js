1ff582f6b91480c1cb0c9f20279a33012c942dfb
Maksim Verkhoturov <sd32@protonmail.com>
2020-08-20 19:57:42 -0600

    updating .gitignore, adding MongoDB connection variable for heroku

--- server.js
+++ server.js
@@ -15,14 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(MONGODB_URI || "mongodb://localhost/budget", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}!`);
 });
