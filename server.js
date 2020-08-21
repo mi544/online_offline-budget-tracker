b44ff1b9d17f854060ad5d67c99e77d95cb15032
Maksim Verkhoturov <sd32@protonmail.com>
2020-08-20 21:17:19 -0600

    initial files

--- server.js
+++ server.js
@@ -15,15 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!`);
 });
