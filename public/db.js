c73f8f82de4755bd554ac49892a6c9764799f1db
Maksim Verkhoturov <sd32@protonmail.com>
2020-08-20 19:51:45 -0600

    minor changes

--- public/db.js
+++ public/db.js
@@ -1,9 +1,9 @@
 let db;
-const request = indexedDB.open('budget', 1);
+const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = ({ target }) => {
     db = target.result;
-    db.createObjectStore('pending', { autoIncrement: true });
+    db.createObjectStore("pending", { autoIncrement: true });
 }
 
 request.onsuccess = ({ target }) => {
@@ -18,29 +18,29 @@ request.onerror = ({ target }) => {
 }
 
 const saveRecord = record => {
-    const transaction = db.transaction(['pending'], 'readwrite');
-    const store = transaction.objectStore('pending');
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
     store.add(record);
 }
 
 const checkDatabase = () => {
-    const transaction = db.transaction(['pending'], 'readwrite');
-    const store = transaction.objectStore('pending');
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
     const getAll = store.getAll();
 
     getAll.onsuccess = () => {
         if (getAll.result.length > 0) {
-            fetch('/api/transaction/bulk', {
+            fetch("/api/transaction/bulk", {
                     method: "POST",
                     body: getAll.result
                 })
                 .then(() => {
-                    const transaction = db.transaction(['pending'], 'readwrite')
-                    const store = transaction.objectStore('pending')
+                    const transaction = db.transaction(["pending"], "readwrite")
+                    const store = transaction.objectStore("pending")
                     store.clear()
                 });
         }
     };
 }
 
-window.addEventListener('online', checkDatabase);
+window.addEventListener("online", checkDatabase);
